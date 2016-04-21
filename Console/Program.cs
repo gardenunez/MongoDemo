008fc636aa0a906a5d1d740d9ca4f26d834304d8
jorge jardines <gardenunez@gmail.com>
2016-04-22 00:47:16 +0200

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace MongoDemo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dbName = "XXXX";
+            string username = "XXXX";
+            string passwd = "XXXX";
+            string server = "XXXX";
+            int port = 0;
+
+            string connectionString = string.Format("mongodb://{0}:{1}@{2}:{3}",
+                username, passwd, server, port);
+            System.Console.WriteLine(connectionString);
+            var client = new MongoClient(connectionString);
+            var databases = client.ListDatabases();
+            
+            var database = client.GetDatabase(dbName);
+
+            var document = new BsonDocument
+            {
+                {"name", string.Format("john doe {0}", Guid.NewGuid())},
+                {"age", new Random().Next(18, 80)},
+                {"city", "Madrid" }
+               
+            };
+
+            var collection = database.GetCollection<BsonDocument>("people");
+            collection.InsertOne(document);
+
+            System.Console.WriteLine("END");
+        }
+    }
+}
